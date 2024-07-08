c72be5e9c1099a9a5a97dde821ad737942549f5c
Jason Lee (Laptop) <jasomslee@gmail.com>
2024-07-08 14:08:02 +0900

    initial commit

---
+++ MNIST_MLP.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
+from sklearn.model_selection import train_test_split
+
+# set seed for repoducibility
+
+
